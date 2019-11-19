dac3593470801c93934227aeea8639304b41c034
HochulKIm <charleshckim@gmail.com>
2019-11-19 10:45:59 -0500

    Youtube Video Follow

--- demo.py
+++ demo.py
@@ -1,4 +1,32 @@
-print("Hello")
-print("Welcome to Hochul")
-print("Hochul Excellent")
-print("Hochul You added two lines in Triad")
+import random
+def generateRandom(upper):
+    """
+
+    :param upper:
+    :return:
+    """
+    r = random.randint(1, upper)
+    return r
+
+def main():
+
+    run = True
+    num1 = generateRandom(10)
+    num2 = generateRandom(10)
+    result = num1*num2
+    while run:
+        ans = input("What is " + str(num1)+ "x "+ str(num2)+ "?")
+
+        if ans.isdigit():
+            if int(ans) == result:
+                print("Correct")
+                run = False
+            else:
+                print("Incorrect! Try again.")
+        else:
+            print("Answer must be a positive number, try again.")
+# global vars
+times =10
+
+for x in range(times):
+    main()
